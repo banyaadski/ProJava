b2becae9c4168ee7ea921b126604aef46beccfe8
banyaadski <89918249+banyaadski@users.noreply.github.com>
2023-10-10 20:45:34 +0300

    first commit

--- hw3/1.js
+++ hw3/1.js
@@ -13,53 +13,67 @@
 // При клике на название продукта отображается список всех отзывов по этому продукту.
 // Возможность удаления отзыва (при нажатии на кнопку "Удалить" рядом с отзывом, данный отзыв удаляется из LocalStorage).
 
-// localStorage.setItem('dima', 'men')
-// localStorage.setItem('dima', 'menw');
-// localStorage.setItem('dima', 'menw2');
-// localStorage.setItem('dima', 'men4');
-// localStorage.setItem('dima', 'men1');
+const prodEl = document.querySelector(".product");
+const revEl = document.querySelector(".review");
+const addBtn = document.querySelector(".addReview");
+const revList = document.querySelector(".review-list");
 
-// // localStorage.removeItem('dima')
+if (localStorage.getItem("reviews")) {
+  revList.innerHTML = localStorage.getItem("reviews");
+}
 
-// let locEl = localStorage.getItem('dima')
-// console.log('Значение из хранилища: ' + locEl);
-
-// // localStorage.clear();
-// console.log('LocalStorage: ' , localStorage);
-
-const prodEl = document.getElementById("product").value;
-const revEl = document.getElementById("review").value;
-const btn = document.querySelector(".addReview");
-const mainEl = document.querySelector(".mainrev");
-
-console.log(prodEl, revEl, btn, mainEl);
-let reviewList = JSON.parse(localStorage.getItem("reviewList")) || [];
-
-const updateList = () => {
-    mainEl.innerHTML = '';
-
-    reviewList.forEach((element) => {
-        const newDivEl = document.createElement("div");
-        const newDivElH1 = document.createElement("h1");
-        const newDivElp = document.createElement("p");
-        newDivElH1.textContent = prodEl;
-        newDivElp.textContent = revEl;
-        mainEl.append(newDivEl);
-        newDivEl.append(newDivElH1)
-        newDivEl.append(newDivElp)
-    });
-    
-    localStorage.setItem('reviewList',JSON.stringify(reviewList))
-};
-btn.addEventListener("click", () => {
-    const newItem = revEl.value;
-
-    if (newItem !=='') {
-        reviewList.push(newItem);
-        revEl.value='';
-        updateList();
-    }
-    updateList();
+addBtn.addEventListener("click", () => {
+  const prodDesc = prodEl.value;
+  const revDesc = revEl.value;
+  if (prodDesc !== "" && revDesc !== "") {
+    const listEl = document.createElement("li");
+    listEl.classList.add("revLi");
+    listEl.innerHTML = `
+    <span class="rev-decs">${prodDesc} <br> ${revDesc}</span>
+        <button class="del-but">Удалить</button>
+    `;
+    revList.appendChild(listEl);
+    prodEl.value = "";
+    revEl.value = "";
+    localStorage.setItem("reviews", revList.innerHTML);
+  }
 });
 
-console.log(reviewList);
+revList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("del-but")) {
+    const listRev = event.target.closest("li");
+    listRev.parentNode.removeChild(listRev);
+
+    localStorage.setItem("reviews", revList.innerHTML);
+  }
+});
+// let reviewList = JSON.parse(localStorage.getItem("reviewList")) || [];
+
+// const updateList = () => {
+//     mainEl.innerHTML = '';
+
+//     reviewList.forEach((element) => {
+//         const newDivEl = document.createElement("div");
+//         const newDivElH1 = document.createElement("h1");
+//         const newDivElp = document.createElement("p");
+//         newDivElH1.textContent = prodEl;
+//         newDivElp.textContent = revEl;
+//         mainEl.append(newDivEl);
+//         newDivEl.append(newDivElH1)
+//         newDivEl.append(newDivElp)
+//     });
+
+//     localStorage.setItem('reviewList',JSON.stringify(reviewList))
+// };
+// btn.addEventListener("click", () => {
+//     const newItem = revEl.value;
+
+//     if (newItem !=='') {
+//         reviewList.push(newItem);
+//         revEl.value='';
+//         updateList();
+//     }
+//     updateList();
+// });
+
+// console.log(reviewList);
